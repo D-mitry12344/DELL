5defab00f36813cf6378c0329f15b69324cbe783
D-mitry12344 <114662273+D-mitry12344@users.noreply.github.com>
2023-11-09 01:19:13 +0300

    upadte css , js

--- js/swiper.js
+++ js/swiper.js
@@ -1,24 +1,3 @@
-
-
-arrayBenner.addEventListener('click',()=>{
-	let arbannerDots = document.querySelectorAll('#dots-banner p');
-	for(let i = 0; i < arbannerDots.length ; i++){
-		if(arbannerDots[i].classList[0] === 'active' && i !== arbannerDots.length-1 ){
-			arbannerDots[i].classList.remove('active')
-			arbannerDots[i+1].classList.add('active');
-			banner.style.marginLeft=String(Number(banner.style.marginLeft.slice(0,-2)) -375) + 'px';
-			break
-		}
-		if(arbannerDots[i].classList[0] === 'active' && i == arbannerDots.length-1 ){
-			arbannerDots[i].classList.remove('active')
-			arbannerDots[0].classList.add('active');
-			banner.style.marginLeft='0px';
-			break
-		}
-	}	 
-	return banner.style.marginLeft;
-})
-
 let startTouch ;
 let newsTouch;
 banner.addEventListener('touchstart',(e)=>{
